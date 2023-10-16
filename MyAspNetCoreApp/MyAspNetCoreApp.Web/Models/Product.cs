b131e0e10ae69b73ffa65fd3ebc46dd488bbf87b
Dilek Kocatürk <dilekkocaturrk@gmail.com>
2023-10-16 22:33:17 +0300

    implement code first

--- MyAspNetCoreApp/MyAspNetCoreApp.Web/Models/Product.cs
+++ MyAspNetCoreApp/MyAspNetCoreApp.Web/Models/Product.cs
@@ -9,7 +9,7 @@
 
 
         public string? Color { get; set; }
-        public int? Width { get; set; }
-        public int? Height { get; set; }
+
+        public string Barcode { get; set; }
     }
 }
